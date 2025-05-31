87a2f378a1e74f0cb594706ce47bff61f3bb5177
Thiago Ananias <thiagoananias@gmail.com>
2025-05-31 18:26:49 -0300

    refactoring

--- src/core/storage/types.ts
+++ src/core/storage/types.ts
@@ -5,7 +5,13 @@ export type StorageTypes = {
   'shared-key': string;
 };
 
-export type JSONValue = string | number | boolean | null | JSONObject | JSONArray;
+export type JSONValue =
+  | string
+  | number
+  | boolean
+  | null
+  | JSONObject
+  | JSONArray;
 export type JSONObject = { [key: string]: JSONValue };
 export type JSONArray = JSONValue[];
 export type Callback<T> = (value: T | null) => void;
